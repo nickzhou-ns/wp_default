59100806e5447af3abe96a050d74ac67e2d7a850
Nick <nick@newstarsec.com.au>
2025-03-01 19:10:14 +1100

    01/03/25 19:10

---
+++ wp-config.php
@@ -0,0 +1,14 @@
+<?php
+define('DB_NAME', 'xxx');
+
+define('DB_USER', 'root');
+
+define('DB_PASSWORD', '');
+
+define('DB_HOST', 'localhost');
+
+define('DB_CHARSET', 'utf8');
+
+define('DB_COLLATE', '');
+define('FS_METHOD', 'direct');
+$table_prefix = 'seoc_1_';
